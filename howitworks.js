85dc8b2bd7e414e1e08e8c4e92f38407e63fe6dd
TABISH KHAN <tabish003@hotmail.com>
2015-09-05 15:12:40 -0700

    passed in html:true option to popover to parse the html inside content of popover

--- howitworks.js
+++ howitworks.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     // console.log("hi");
     // $("#howitworksdiv").toggle();
     // console.log($('[data-toggle="popover"]'));
-    $('[data-toggle="popover"]').popover({title:"Calculating Password Strength", content:'A password is considered strong when:<br>It has more than 8 characters,,It has at least one lower case characterIt has at least one upper case character,It has at least one non alphanumeric character,It has more than 12 charactersEach of these criteria gives the password 1 point. A password is considered:0 points: terrible1 points: weak2 points: meh meh3 points: good4 points: strong5 points: awesome!'});
-
+    $('[data-toggle="popover"]').popover({title:"Calculating Password Strength", content:'A password is considered strong when:<br> <ul><li>It has more than 8 characters</li><li>It has at least one lower case character</li><li>It has at least one upper case character</li><li>It has at least one non alphanumeric character</li><li>It has more than 12 characters</li></ul>Each of these criteria gives the password <strong>1 point</strong>.<br><br> A password is considered: <ul><li>0 points: terrible</li><li>1 points: weak</li><li>2 points: meh meh</li><li>3 points: good</li><li>4 points: strong</li><li>5 points: awesome!</li></ul>', html: true});
+    // $('[data-toggle="popover"]').popover({title:"Calculating Password Strength", content: 'data-template'});
 });
 
